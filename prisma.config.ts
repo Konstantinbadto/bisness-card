0118a246bdd87d59b06907736c3a5635da4cbfe5
BadTom <ajrih.k.i.23@gmail.com>
2026-09-06 20:22:51 +0300

    fix: миграция на Prisma ORM 71

--- prisma.config.ts
+++ prisma.config.ts
@@ -19,4 +19,12 @@ export default defineConfig({
     // в .env одно и то же значение в обеих переменных.
     url: env('DIRECT_URL'),
   },
+  import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL || '',
+    directUrl: process.env.DIRECT_URL || process.env.DATABASE_URL || '',
+  },
+});
 });
