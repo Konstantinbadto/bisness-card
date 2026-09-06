055bcd53d7cf9d080b66790360ab1fb1c03b9450
BadTom <ajrih.k.i.23@gmail.com>
2026-09-06 21:00:17 +0300

    fix: миграция на Prisma ORM 12-

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Включаем CORS для всех источников
   app.enableCors();
+
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`🚀 Визитка запущена: http://localhost:${port}`);
-  console.log(`🔎 GraphQL Playground: http://localhost:${port}/graphql`);
+
+  // Передаем '0.0.0.0', чтобы слушать все сетевые интерфейсы внутри контейнера
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Сервер успешно запущен на порту: ${port}`);
+  console.log(`🔎 GraphQL эндпоинт доступен по адресу: /graphql`);
 }
 
 bootstrap();
